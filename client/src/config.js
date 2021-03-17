210b8964bbdbc064b255949a93e4bef3270b7152
Agustin Bessone <bessone98@gmail.com>
2021-03-17 15:11:10 -0300

    cambio

--- client/src/config.js
+++ client/src/config.js
@@ -3,4 +3,4 @@ export const IMAGES = "https://image.tmdb.org/t/p/w1280"
 export const SEARCH = "https://api.themoviedb.org/3/search/movie?&api_key=f3e32df54960b38fb814c3b24b0097d0&query="
 
 
-export const SERVER = "https://total-pelis.herokuapp.com/"
+export const SERVER = "https://total-pelis.herokuapp.com"
