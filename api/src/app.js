7338a952641855ef18656ea261750cbaf0725f49
Agustin Bessone <bessone98@gmail.com>
2021-03-17 14:52:41 -0300

    tercer cambio

--- api/src/app.js
+++ api/src/app.js
@@ -3,13 +3,11 @@ const app = express()
 const morgan = require('morgan')
 const mongoose = require('mongoose')
 var cors = require('cors')
+require('dotenv').config()
 
-const user = 'tito';
-const pass = 'PL56Fgmf3uKZeeas';
-const dbname = 'total-pelis'
-const url =`mongodb+srv://${user}:${pass}@cluster0.qf6tz.mongodb.net/${dbname}?retryWrites=true&w=majority`;
+const url =`mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.qf6tz.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
 
- 
+
 
 // conexion con bd
 mongoose.connect(url, {
